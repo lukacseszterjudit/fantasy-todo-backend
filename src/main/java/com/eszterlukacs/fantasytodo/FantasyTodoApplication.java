7b78617c9cd2779b2a6091b5a532f908dd009409
Eszter Lukács <lukacseszterjudit@gmail.com>
2022-02-10 14:30:41 +0100

    project initialized with spring initializr

---
+++ src/main/java/com/eszterlukacs/fantasytodo/FantasyTodoApplication.java
@@ -0,0 +1,13 @@
+package com.eszterlukacs.fantasytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eszterlukacs/fantasytodo/FantasyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eszterlukacs.fantasytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
