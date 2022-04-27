2a04f5dab4555852e3e332ff435176f3232d3e36
Eszter Lukács <lukacseszterjudit@gmail.com>
2022-04-27 20:56:19 +0200

    find task and reward by id

--- src/main/java/com/eszterlukacs/fantasytodo/controller/RewardController.java
+++ src/main/java/com/eszterlukacs/fantasytodo/controller/RewardController.java
@@ -1,9 +1,11 @@
 package com.eszterlukacs.fantasytodo.controller;
 
+import com.eszterlukacs.fantasytodo.exception.ResourceNotFoundException;
 import com.eszterlukacs.fantasytodo.model.Reward;
 import com.eszterlukacs.fantasytodo.model.Task;
 import com.eszterlukacs.fantasytodo.repository.RewardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,11 @@ public class RewardController {
     @PostMapping("/rewards")
     public Reward createReward(@RequestBody Reward reward){
         return rewardRepository.save(reward);
+    }
+
+    @GetMapping("/rewards/{id}")
+    public ResponseEntity<Reward> getRewardById(@PathVariable Long id) {
+        Reward reward = rewardRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Reward does not exist with id: " + id));
+        return ResponseEntity.ok(reward);
     }
 }
--- src/main/java/com/eszterlukacs/fantasytodo/controller/TaskController.java
+++ src/main/java/com/eszterlukacs/fantasytodo/controller/TaskController.java
@@ -1,8 +1,10 @@
 package com.eszterlukacs.fantasytodo.controller;
 
+import com.eszterlukacs.fantasytodo.exception.ResourceNotFoundException;
 import com.eszterlukacs.fantasytodo.model.Task;
 import com.eszterlukacs.fantasytodo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +25,11 @@ public class TaskController {
     @PostMapping("/tasks")
     public Task createTask(@RequestBody Task task){
         return taskRepository.save(task);
+    }
+
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task does not exist with id: " + id));
+        return ResponseEntity.ok(task);
     }
 }
